5e3b6bb18040b75425f2830bbcbbf7bc080aefee
Kevin <stradtkt22@gmail.com>
2019-06-21 13:41:53 -0400

    Fetch all posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import PostList from './components/PostList';
 
 class App extends Component {
     render() {
         return (
-            <div></div>
+            <div className="ui container">
+                <PostList/>
+            </div>
         );
     }
 }
